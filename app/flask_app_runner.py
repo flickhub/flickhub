a4185bd37d4bf044bd52c55a770c5b8db5c7409b
suvo696 <18bec062@smvdu.ac.in>
2020-05-11 22:20:41 +0530

    Update flask_app_runner.py

--- app/flask_app_runner.py
+++ app/flask_app_runner.py
@@ -15,15 +15,7 @@ def submit():
             return render_template('index1.html',u=url_name3,ie=img3,le=l,nam=name3,g=genre3)    
         else:
             return redirect(url_for('home'))
-        mvname=request.form["mv_name1"]
-        if(mvname!=""):
-            print(mvname)
-            m="%"+mvname+"%"
-            #db search
-            url_name3,img3,l,name3,genre3=see(m)
-            return redirect('index1.html',u=url_name3,ie=img3,le=l,nam=name3,g=genre3)    
-        else:
-            return redirect(url_for('home'))
+       
         
 app.run(debug=True)
 
