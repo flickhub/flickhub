9fa3ef0e6e50ab11f77c9e6c3bb7a69440ca2e3d
suvo696 <18bec062@smvdu.ac.in>
2020-05-13 07:09:37 +0530

    Update flask_app_runner.py

--- app/flask_app_runner.py
+++ app/flask_app_runner.py
@@ -15,10 +15,19 @@ def submit():
             return render_template('index1.html',u=url_name3,ie=img3,le=l,nam=name3,g=genre3)    
         else:
             return redirect(url_for('home'))
-       
-        
+@app.route("/about")
+def about():
+    return render_template('about.html')  
+@app.route("/contact")
+def contact():
+    return render_template('contact.html')
+@app.route("/tos")
+def tos():
+    return render_template('tos.html')       
+@app.route("/pp")
+def pp():
+    return render_template('pp.html')
 app.run(debug=True)
-
 
 
 
