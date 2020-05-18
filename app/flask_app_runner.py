4b122f6ef8bca69437e92325bb477f875d0038b7
suvo696 <18bec062@smvdu.ac.in>
2020-05-18 18:57:07 +0530

    Update flask_app_runner.py

--- app/flask_app_runner.py
+++ app/flask_app_runner.py
@@ -3,7 +3,7 @@ from db_initiate import see
 app = Flask(__name__)    
 @app.route("/",methods=["GET","POST"])
 def home():
-    return render_template('index.html')  
+    return render_template('index2.html')  
 @app.route("/submit",methods=["GET","POST"])
 def submit():
         mvname=request.form["mv_name"]
@@ -13,9 +13,12 @@ def submit():
             
             #db search
             url_name3,img3,l,name3,genre3=see(m)
-            return render_template('index1.html',u=url_name3,ie=img3,le=l,nam=name3,g=genre3,nam1=m)    
+            if(len(name3)!=0):
+                return render_template('index1.html',u=url_name3,ie=img3,le=l,nam=name3,g=genre3,nam1=m)    
+            else:
+                return render_template('blank1.html',nam1=m)
         else:
-            return render_template('blank1.html')
+            return render_template('blank1.html',nam1=m)
 @app.route("/about")
 def about():
     return render_template('about.html')  
@@ -23,8 +26,8 @@ def about():
 def contact():
     return render_template('contact.html')
 
-if __name__ == "__main__":
-	app.run(debug=True)
+
+app.run(debug=True)
 
 
 
