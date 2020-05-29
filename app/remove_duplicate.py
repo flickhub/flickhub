e643b506effdcb0d1ba7c92db9e49c44a849ebe9
suvo696 <18bec062@smvdu.ac.in>
2020-05-29 20:38:54 +0530

    Update remove_duplicate.py

--- app/remove_duplicate.py
+++ app/remove_duplicate.py
@@ -1,219 +1,436 @@
 
-def dup(n2,g2,s2,u2):
-		
-		img=[]
-		name22=n2
-		genre22=g2
-		site22=s2
-		url_name22=u2
-		l1=[]
-		l2=[]
-		l3=[]
-		l4=[]
-		l5=[]
-		l6=[]
-		l7=[]
-		l8=[]
-		l9=[]
-		
-		
-		
-		name1=[]
-		name2=[]
-		name3=[]
-		name4=[]
-		name5=[]
-		name6=[]
-		name7=[]
-		name8=[]
-		name9=[]
-		
-		url1=[]
-		url2=[]
-		url3=[]
-		url4=[]
-		url5=[]
-		url6=[]
-		url7=[]
-		url8=[]
-		url9=[]
-		
-		site1=[]
-		site2=[]
-		site3=[]
-		site4=[]
-		site5=[]
-		site6=[]
-		site7=[]
-		site8=[]
-		site9=[]
-		
-		genre1=[]
-		genre2=[]
-		genre3=[]
-		genre4=[]
-		genre5=[]
-		genre6=[]
-		genre7=[]
-		genre8=[]
-		genre9=[]
-		for x in range(len(site22)):
-			if(site22[x]=='NETFLIX'):
-				name1.append(name22[x])
-				url1.append(url_name22[x])
-				site1.append(site22[x])
-				genre1.append(genre22[x])
-				l1.append(x)
-			elif(site22[x]=='PRIME'):
-				name2.append(name22[x])
-				url2.append(url_name22[x])
-				site2.append(site22[x])
-				genre2.append(genre22[x])
-				l2.append(x)
-			elif(site22[x]=='HOTSTAR'):
-				name3.append(name22[x])
-				url3.append(url_name22[x])
-				site3.append(site22[x])
-				genre3.append(genre22[x])
-				l3.append(x)
-			elif(site22[x]=='SONYLIV'):
-				name4.append(name22[x])
-				url4.append(url_name22[x])
-				site4.append(site22[x])
-				genre4.append(genre22[x])
-				l4.append(x)
-			elif(site22[x]=='ZEE5'):
-				name5.append(name22[x])
-				url5.append(url_name22[x])
-				site5.append(site22[x])
-				genre5.append(genre22[x])
-				l5.append(x)
-			elif(site22[x]=='VOOT'):
-				name6.append(name22[x])
-				url6.append(url_name22[x])
-				site6.append(site22[x])
-				genre6.append(genre22[x])
-				l6.append(x)
-			elif(site22[x]=='ALTBALAJI'):
-				name7.append(name22[x])
-				url7.append(url_name22[x])
-				site7.append(site22[x])
-				genre7.append(genre22[x])
-				l7.append(x)
-			elif(site22[x]=='EROSNOW'):
-				name8.append(name22[x])
-				url8.append(url_name22[x])
-				site8.append(site22[x])
-				genre8.append(genre22[x])
-				l8.append(x)
-			elif(site22[x]=='VIU'):
-				name9.append(name22[x])
-				url9.append(url_name22[x])
-				site9.append(site22[x])
-				genre9.append(genre22[x])
-				l8.append(x)
+def dup(n2,g2,s2,u2,im2,r2,y2,v2,ge2,c2,p2):
+        
+        img=[]
+        name22=n2
+        genre22=g2
+        site22=s2
+        url_name22=u2
+        im22=im2
+        r22=r2
+        y22=y2
+        v22=v2
+        ge22=ge2
+        c22=c2
+        p22=p2
+        
+        l1=[]
+        l2=[]
+        l3=[]
+        l4=[]
+        l5=[]
+        l6=[]
+        l7=[]
+        l8=[]
+        l9=[]
+        
+        
+        
+        name1=[]
+        name2=[]
+        name3=[]
+        name4=[]
+        name5=[]
+        name6=[]
+        name7=[]
+        name8=[]
+        name9=[]
+        
+        url1=[]
+        url2=[]
+        url3=[]
+        url4=[]
+        url5=[]
+        url6=[]
+        url7=[]
+        url8=[]
+        url9=[]
+        
+        site1=[]
+        site2=[]
+        site3=[]
+        site4=[]
+        site5=[]
+        site6=[]
+        site7=[]
+        site8=[]
+        site9=[]
+        
+        genre1=[]
+        genre2=[]
+        genre3=[]
+        genre4=[]
+        genre5=[]
+        genre6=[]
+        genre7=[]
+        genre8=[]
+        genre9=[]
+        
+        imge1=[]
+        imge2=[]
+        imge3=[]
+        imge4=[]
+        imge5=[]
+        imge6=[]
+        imge7=[]
+        imge8=[]
+        imge9=[]
+        
+        rate1=[]
+        rate2=[]
+        rate3=[]
+        rate4=[]
+        rate5=[]
+        rate6=[]
+        rate7=[]
+        rate8=[]
+        rate9=[]
+        
+        year1=[]
+        year2=[]
+        year3=[]
+        year4=[]
+        year5=[]
+        year6=[]
+        year7=[]
+        year8=[]
+        year9=[]
+        
+        vote1=[]
+        vote2=[]
+        vote3=[]
+        vote4=[]
+        vote5=[]
+        vote6=[]
+        vote7=[]
+        vote8=[]
+        vote9=[]
+        
+        gen1=[]
+        gen2=[]
+        gen3=[]
+        gen4=[]
+        gen5=[]
+        gen6=[]
+        gen7=[]
+        gen8=[]
+        gen9=[]
+        
+        cast1=[]
+        cast2=[]
+        cast3=[]
+        cast4=[]
+        cast5=[]
+        cast6=[]
+        cast7=[]
+        cast8=[]
+        cast9=[]
+        
+        plat1=[]
+        plat2=[]
+        plat3=[]
+        plat4=[]
+        plat5=[]
+        plat6=[]
+        plat7=[]
+        plat8=[]
+        plat9=[]
+        
+        for x in range(len(site22)):
+            if(site22[x]=='NETFLIX'):
+                name1.append(name22[x])
+                url1.append(url_name22[x])
+                site1.append(site22[x])
+                genre1.append(genre22[x])
+                imge1.append(im22[x])
+                rate1.append(r22[x])
+                year1.append(y22[x])
+                vote1.append(v22[x])
+                gen1.append(ge22[x])
+                cast1.append(c22[x])
+                plat1.append(p22[x])
+                l1.append(x)
+            
+            elif(site22[x]=='PRIME'):
+                name2.append(name22[x])
+                url2.append(url_name22[x])
+                site2.append(site22[x])
+                genre2.append(genre22[x])
+                imge2.append(im22[x])
+                rate2.append(r22[x])
+                year2.append(y22[x])
+                vote2.append(v22[x])
+                gen2.append(ge22[x])
+                cast2.append(c22[x])
+                plat2.append(p22[x])
+                l2.append(x)
+            
+            elif(site22[x]=='HOTSTAR'):
+                name3.append(name22[x])
+                url3.append(url_name22[x])
+                site3.append(site22[x])
+                genre3.append(genre22[x])
+                imge3.append(im22[x])
+                rate3.append(r22[x])
+                year3.append(y22[x])
+                vote3.append(v22[x])
+                gen3.append(ge22[x])
+                cast3.append(c22[x])
+                plat3.append(p22[x])
+                l3.append(x)
+            
+            elif(site22[x]=='SONYLIV'):
+                name4.append(name22[x])
+                url4.append(url_name22[x])
+                site4.append(site22[x])
+                genre4.append(genre22[x])
+                imge4.append(im22[x])
+                rate4.append(r22[x])
+                year4.append(y22[x])
+                vote4.append(v22[x])
+                gen4.append(ge22[x])
+                cast4.append(c22[x])
+                plat4.append(p22[x])
+                l4.append(x)
+            elif(site22[x]=='ZEE5'):
+                name5.append(name22[x])
+                url5.append(url_name22[x])
+                site5.append(site22[x])
+                genre5.append(genre22[x])
+                imge5.append(im22[x])
+                rate5.append(r22[x])
+                year5.append(y22[x])
+                vote5.append(v22[x])
+                gen5.append(ge22[x])
+                cast5.append(c22[x])
+                plat5.append(p22[x])
+                l5.append(x)
+            elif(site22[x]=='VOOT'):
+                name6.append(name22[x])
+                url6.append(url_name22[x])
+                site6.append(site22[x])
+                genre6.append(genre22[x])
+                imge6.append(im22[x])
+                rate6.append(r22[x])
+                year6.append(y22[x])
+                vote6.append(v22[x])
+                gen6.append(ge22[x])
+                cast6.append(c22[x])
+                plat6.append(p22[x])
+                l6.append(x)
+            elif(site22[x]=='ALTBALAJI'):
+                name7.append(name22[x])
+                url7.append(url_name22[x])
+                site7.append(site22[x])
+                genre7.append(genre22[x])
+                imge7.append(im22[x])
+                rate7.append(r22[x])
+                year7.append(y22[x])
+                vote7.append(v22[x])
+                gen7.append(ge22[x])
+                cast7.append(c22[x])
+                plat7.append(p22[x])
+                l7.append(x)
+            elif(site22[x]=='EROSNOW'):
+                name8.append(name22[x])
+                url8.append(url_name22[x])
+                site8.append(site22[x])
+                genre8.append(genre22[x])
+                imge8.append(im22[x])
+                rate8.append(r22[x])
+                year8.append(y22[x])
+                vote8.append(v22[x])
+                gen8.append(ge22[x])
+                cast8.append(c22[x])
+                plat8.append(p22[x])
+                l8.append(x)
+            elif(site22[x]=='VIU'):
+                name9.append(name22[x])
+                url9.append(url_name22[x])
+                site9.append(site22[x])
+                genre9.append(genre22[x])
+                imge9.append(im22[x])
+                rate9.append(r22[x])
+                year9.append(y22[x])
+                vote9.append(v22[x])
+                gen9.append(ge22[x])
+                cast9.append(c22[x])
+                plat9.append(p22[x])
+                l8.append(x)
 
-		
-		
-		name=[]
-		url=[]
-		genre=[]
-		site=[]
-		
-		l2=[]
-		name_pure=list(dict.fromkeys(name1))
-		for j in name_pure:
-			l2.append(name1.index(j))
-		for i in l2:	
-			name.append(name1[i])
-			url.append(url1[i])
-			genre.append(genre1[i])
-			site.append(site1[i])
-			img.append('1.png')
-		
-		
-		l2=[]
-		name_pure=list(dict.fromkeys(name2))
-		for j in name_pure:
-			l2.append(name2.index(j))
-		for i in l2:	
-			name.append(name2[i])
-			url.append(url2[i])
-			genre.append(genre2[i])
-			site.append(site2[i])
-			img.append('2.png')
-		
-		
-		l2=[]
-		name_pure=list(dict.fromkeys(name3))
-		for j in name_pure:
-			l2.append(name3.index(j))
-		for i in l2:	
-			name.append(name3[i])
-			url.append(url3[i])
-			genre.append(genre3[i])
-			site.append(site3[i])
-			img.append('8.png')
-		l2=[]
-		name_pure=list(dict.fromkeys(name4))
-		for j in name_pure:
-			l2.append(name4.index(j))
-		for i in l2:	
-			name.append(name4[i])
-			url.append(url4[i])
-			genre.append(genre4[i])
-			site.append(site4[i])
-			img.append('4.png')
-		l2=[]
-		name_pure=list(dict.fromkeys(name5))
-		for j in name_pure:
-			l2.append(name5.index(j))
-		for i in l2:	
-			name.append(name5[i])
-			url.append(url5[i])
-			genre.append(genre5[i])
-			site.append(site5[i])
-			img.append('6.png')
-		l2=[]
-		name_pure=list(dict.fromkeys(name6))
-		for j in name_pure:
-			l2.append(name6.index(j))
-		for i in l2:	
-			name.append(name6[i])
-			url.append(url6[i])
-			genre.append(genre6[i])
-			site.append(site6[i])
-			img.append('7.png')
-		l2=[]
-		name_pure=list(dict.fromkeys(name7))
-		for j in name_pure:
-			l2.append(name7.index(j))
-		for i in l2:	
-			name.append(name7[i])
-			url.append(url7[i])
-			genre.append(genre7[i])
-			site.append(site7[i])
-			img.append('5.png')
-		l2=[]
-		name_pure=list(dict.fromkeys(name8))
-		for j in name_pure:
-			l2.append(name8.index(j))
-		for i in l2:	
-			name.append(name8[i])
-			url.append(url8[i])
-			genre.append(genre8[i])
-			site.append(site8[i])
-			img.append('3.png')
-		l2=[]
-		name_pure=list(dict.fromkeys(name9))
-		for j in name_pure:
-			l2.append(name9.index(j))
-		for i in l2:	
-			name.append(name9[i])
-			url.append(url9[i])
-			genre.append(genre9[i])
-			site.append(site9[i])
-			img.append('9.png')
-		
-			
-		return (url,img,site,name,genre) 
+        
+        
+        name=[]
+        url=[]
+        genre=[]
+        site=[]
+        imga=[]
+        rat=[]
+        yea=[]
+        vot=[]
+        genree=[]
+        cst=[]
+        plate=[]
+        
+        l2=[]
+        name_pure=list(dict.fromkeys(name1))
+        for j in name_pure:
+            l2.append(name1.index(j))
+        for i in l2:    
+            name.append(name1[i])
+            url.append(url1[i])
+            genre.append(genre1[i])
+            site.append(site1[i])
+            img.append('10.png')
+            imga.append(imge1[i])
+            rat.append(rate1[i])
+            yea.append(year1[i])
+            vot.append(vote1[i])
+            genree.append(gen1[i])
+            cst.append(cast1[i])
+            plate.append(plat1[i])
+        
+        l2=[]
+        name_pure=list(dict.fromkeys(name2))
+        for j in name_pure:
+            l2.append(name2.index(j))
+        for i in l2:    
+            name.append(name2[i])
+            url.append(url2[i])
+            genre.append(genre2[i])
+            site.append(site2[i])
+            img.append('29.png')
+            imga.append(imge2[i])
+            rat.append(rate2[i])
+            yea.append(year2[i])
+            vot.append(vote2[i])
+            genree.append(gen2[i])
+            cst.append(cast2[i])
+            plate.append(plat2[i])
+        
+        l2=[]
+        name_pure=list(dict.fromkeys(name3))
+        for j in name_pure:
+            l2.append(name3.index(j))
+        for i in l2:    
+            name.append(name3[i])
+            url.append(url3[i])
+            genre.append(genre3[i])
+            site.append(site3[i])
+            img.append('83.png')
+            imga.append(imge3[i])
+            rat.append(rate3[i])
+            yea.append(year3[i])
+            vot.append(vote3[i])
+            genree.append(gen3[i])
+            cst.append(cast3[i])
+            plate.append(plat3[i])
+        l2=[]
+        name_pure=list(dict.fromkeys(name4))
+        for j in name_pure:
+            l2.append(name4.index(j))
+        for i in l2:    
+            name.append(name4[i])
+            url.append(url4[i])
+            genre.append(genre4[i])
+            site.append(site4[i])
+            img.append('40.png')
+            imga.append(imge4[i])
+            rat.append(rate4[i])
+            yea.append(year4[i])
+            vot.append(vote4[i])
+            genree.append(gen4[i])
+            cst.append(cast4[i])
+            plate.append(plat4[i])
+            
+        l2=[]
+        name_pure=list(dict.fromkeys(name5))
+        for j in name_pure:
+            l2.append(name5.index(j))
+        for i in l2:    
+            name.append(name5[i])
+            url.append(url5[i])
+            genre.append(genre5[i])
+            site.append(site5[i])
+            img.append('60.png')
+            imga.append(imge5[i])
+            rat.append(rate5[i])
+            yea.append(year5[i])
+            vot.append(vote5[i])
+            genree.append(gen5[i])
+            cst.append(cast5[i])
+            plate.append(plat5[i])
+        l2=[]
+        name_pure=list(dict.fromkeys(name6))
+        for j in name_pure:
+            l2.append(name6.index(j))
+        for i in l2:    
+            name.append(name6[i])
+            url.append(url6[i])
+            genre.append(genre6[i])
+            site.append(site6[i])
+            img.append('70.png')
+            imga.append(imge6[i])
+            rat.append(rate6[i])
+            yea.append(year6[i])
+            vot.append(vote6[i])
+            genree.append(gen6[i])
+            cst.append(cast6[i])
+            plate.append(plat6[i])
+            
+        l2=[]
+        name_pure=list(dict.fromkeys(name7))
+        for j in name_pure:
+            l2.append(name7.index(j))
+        for i in l2:    
+            name.append(name7[i])
+            url.append(url7[i])
+            genre.append(genre7[i])
+            site.append(site7[i])
+            img.append('50.png')
+            imga.append(imge7[i])
+            rat.append(rate7[i])
+            yea.append(year7[i])
+            vot.append(vote7[i])
+            genree.append(gen7[i])
+            cst.append(cast7[i])
+            plate.append(plat7[i])
+            
+        l2=[]
+        name_pure=list(dict.fromkeys(name8))
+        for j in name_pure:
+            l2.append(name8.index(j))
+        for i in l2:    
+            name.append(name8[i])
+            url.append(url8[i])
+            genre.append(genre8[i])
+            site.append(site8[i])
+            img.append('30.png')
+            imga.append(imge8[i])
+            rat.append(rate8[i])
+            yea.append(year8[i])
+            vot.append(vote8[i])
+            genree.append(gen8[i])
+            cst.append(cast8[i])
+            plate.append(plat8[i])
+            
+        l2=[]
+        name_pure=list(dict.fromkeys(name9))
+        for j in name_pure:
+            l2.append(name9.index(j))
+        for i in l2:    
+            name.append(name9[i])
+            url.append(url9[i])
+            genre.append(genre9[i])
+            site.append(site9[i])
+            img.append('90.png')
+            imga.append(imge9[i])
+            rat.append(rate9[i])
+            yea.append(year9[i])
+            vot.append(vote9[i])
+            genree.append(gen9[i])
+            cst.append(cast9[i])
+            plate.append(plat9[i])
+        
+            
+        return (url,img,site,name,genre,imga,rat,yea,vot,genree,cst,plate) 
