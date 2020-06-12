83117b0d643ec9a5def5c05b980fc98d5ab28fa4
suvo696 <18bec062@smvdu.ac.in>
2020-06-12 18:58:16 +0530

    Update flask_app_runner.py

--- app/flask_app_runner.py
+++ app/flask_app_runner.py
@@ -1,41 +1,40 @@
-from flask import Flask, render_template,request,redirect, url_for
+from flask import Flask, render_template,request,redirect, url_for,jsonify
 from db_initiate import see
-app = Flask(__name__)    
-@app.route("/",methods=["GET","POST"])
-def home():
-    return render_template('index.html')  
-@app.route("/submit",methods=["GET","POST"])
-def submit():
-        count=0
-        mvname=request.form["mv_name"]
-        for i in mvname:
-            if(i==' '):
-                count=count+1
-        l=len(mvname)        
-        if(mvname!="" and l!=count):
-            print(mvname)
-            m=mvname.lstrip()
-            
-            #db search
-            url_name3,img3,l,name3,genre3,im3,rate3,yr3,vt3,gen3,cst3,pl3=see(m)
-            if(len(name3)!=0):
-                
-                return render_template('index1.html',u=url_name3,ie=img3,le=l,nam=name3,g=gen3,nam1=m,im=im3,rate=rate3,year=yr3,vote=vt3,cast=cst3,plat=pl3)    
-            else:
-                return render_template('blank1.html',nam1=m)
-        else:
-            return render_template('blank1.html')
-@app.route("/about")
-def about():
-    return render_template('about.html')  
-@app.route("/contact")
-def contact():
-    return render_template('contact.html')
-@app.route("/feedback")
-def feedback():
-    return render_template('feedback.html')
+app = Flask(__name__)	 
 
-app.run(debug=True)
+@app.route("/submit/<mvname>",methods=["POST","GET"])
+def submit(mvname):
+		count=0
+		
+            
+		if (request.method == 'GET'): 
+			
+			for i in mvname:
+				if(i==' '):
+					count=count+1
+			l=len(mvname)		 
+			if(mvname!="" and l!=count):
+				print(mvname)
+				m=mvname.lstrip()
+				
+				#db search
+				result={}
+				url_name3,img3,l,name3,genre3,im3,rate3,yr3,vt3,gen3,cst3,pl3=see(m)
+				result["Urlname"]=url_name3
+				result["Image"]=img3
+				result["Name"]=name3
+				result["Image"]=im3
+				result["Genre"]=gen3
+				result["Rate"]=rate3
+				result["Year"]=yr3
+				result["Votes"]=vt3
+				result["Cast"]=cst3
+				result["Plot"]=pl3
+				
+					
+				return jsonify(result)
+			
+app.run(debug=True,host='0.0.0.0')
 
 
 
