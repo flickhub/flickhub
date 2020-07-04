26f304127f4a09d56e094b396b775638323531ae
Parth <parth.verma96@gmail.com>
2020-07-04 16:03:07 +0530

    changes required for server

--- app/flask_app_runner.py
+++ app/flask_app_runner.py
@@ -6,53 +6,52 @@ from feedback import feed_back
 from title import title_render
 app = Flask(__name__)	 
 
-def run():
+@app.route("/",methods=["POST"])
+def land_page():
+    resp_data0=random1()
     
-    @app.route("/",methods=["POST"])
-    def land_page():
-        resp_data0=random1()
+    return jsonify({'data': resp_data0})
+@app.route("/submit",methods=["POST"])
+def submit():
+    count=0
+    mvname = request.json['mv_name']
+    for i in mvname:
+        if(i==' '):
+            count=count+1
+    l=len(mvname)		 
+    if(mvname!="" and l!=count):
+        m=mvname.lstrip()
+        print(m)
+        #db search
+        result={}
+        resp_data=see(m)
         
-        return jsonify({'data': resp_data0})
-    @app.route("/submit",methods=["POST"])
-    def submit():
-        count=0
-        mvname = request.json['mv_name']
-        for i in mvname:
-            if(i==' '):
-                count=count+1
-        l=len(mvname)		 
-        if(mvname!="" and l!=count):
-            m=mvname.lstrip()
-            print(m)
-            #db search
-            result={}
-            resp_data=see(m)
-            
-            return jsonify({'data': resp_data})
-    @app.route("/filter",methods=["POST"])	
-    def filter():
-        filter1={}
-        filter1["filters"] = request.json['filters']
-        print(filter1)
-        resp_data1=filter_fr_all(filter1)
-            
+        return jsonify({'data': resp_data})
+@app.route("/filter",methods=["POST"])	
+def filter():
+    filter1={}
+    filter1["filters"] = request.json['filters']
+    print(filter1)
+    resp_data1=filter_fr_all(filter1)
         
-        return jsonify({'data': resp_data1})
+    
+    return jsonify({'data': resp_data1})
 
-    @app.route("/feedback",methods=["POST"])	
-    def feedback():
-        feedback={}
-        feedback["feedback"] = request.json['feedback']
-        feed_back(feedback)
-        
-        return("1")    
-    @app.route("/title/<id_mov1>",methods=["POST"])	
-    def title(id_mov1):
-        resp_data=title_render(id_mov1)
-        return jsonify({'data': resp_data})  
-    app.run(debug=True,host='0.0.0.0')
+@app.route("/feedback",methods=["POST"])	
+def feedback():
+    feedback={}
+    feedback["feedback"] = request.json['feedback']
+    feed_back(feedback)
     
-run()			
+    return("1")    
+@app.route("/title/<id_mov1>",methods=["POST"])	
+def title(id_mov1):
+    resp_data=title_render(id_mov1)
+    return jsonify({'data': resp_data})  
+    
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0')
+			
 
     
 
